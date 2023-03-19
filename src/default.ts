7e50c1ae34b571ae3c9d261a269cd73f051b021f
ok <ok@ok.com>
2023-03-19 16:52:46 +0800

    test

--- src/default.ts
+++ src/default.ts
@@ -4,10 +4,9 @@ export const defaultSetting = {
   openaiAPIKey: "",
   openaiAPITemperature: 60,
   systemRule: ""
-}
+};
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
-- 由 [ourongxing](https://github.com/ourongxing) 修改自 [Diu](https://github.com/ddiu8081/chatgpt-demo)，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 本程序旨在运用人工智能技术解决在日常生活和工作中遇到的问题。
+- 由ChatGPT[OpenAI API (gpt-3.5-turbo)] 提供技术支持。
+- `;
