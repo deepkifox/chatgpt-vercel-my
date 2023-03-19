d4ce2aadfce6a8fb490966366c8f5350be20e83e
ok <ok@ok.com>
2023-03-19 17:00:48 +0800

    test

--- src/default.ts
+++ src/default.ts
@@ -7,4 +7,5 @@ export const defaultSetting = {
 };
 
 export const defaultMessage = `
-- 本程序旨在运用人工智能技术解决在日常生活和工作中遇到的问题。由ChatGPT提供API接口(gpt-3.5-turbo)。请在下方输入框中输入你需要解决的问题，支持连续对话。`;
+- 本程序旨在运用人工智能技术解决在日常生活和工作中遇到的问题。由ChatGPT提供API接口，版本为gpt-3.5-turbo。
+- 请在下方输入框中输入你需要解决的问题，支持连续对话。`;
